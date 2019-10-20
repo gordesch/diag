853bc107abeec700437458401bbe343ce014f87c
Gordesch <gordesch@users.noreply.github.com>
2019-10-20 02:48:28 +0200

    pcs

--- empl.R
+++ empl.R
@@ -61,6 +61,7 @@ empl <- empl90 %>%
   full_join(empl10) %>%
   full_join(empl15)
 
+# Paris
 empl75 <- empl %>%
   summarise(C90_ACT1564_CS1 = sum(AT90TAG, na.rm = TRUE) / sum(AT90TA, na.rm = TRUE) * 100,
             C99_ACT1564_CS1 = sum(AT99TAG, na.rm = TRUE) / sum(AT99TA, na.rm = TRUE) * 100,
@@ -69,5 +70,116 @@ empl75 <- empl %>%
             C90_ACT1564_CS2 = sum(AT90TAR, na.rm = TRUE) / sum(AT90TA, na.rm = TRUE) * 100,
             C99_ACT1564_CS2 = sum(AT99TAR, na.rm = TRUE) / sum(AT99TA, na.rm = TRUE) * 100,
             C10_ACT1564_CS2 = sum(C10_ACT1564_CS2, na.rm = TRUE) / sum(C10_ACT1564, na.rm = TRUE) * 100,
-            C15_ACT1564_CS2 = sum(C15_ACT1564_CS2, na.rm = TRUE) / sum(C15_ACT1564, na.rm = TRUE) * 100,) %>%
+            C15_ACT1564_CS2 = sum(C15_ACT1564_CS2, na.rm = TRUE) / sum(C15_ACT1564, na.rm = TRUE) * 100,
+            C90_ACT1564_CS3 = sum(AT90TCA, na.rm = TRUE) / sum(AT90TA, na.rm = TRUE) * 100,
+            C99_ACT1564_CS3 = sum(AT99TCA, na.rm = TRUE) / sum(AT99TA, na.rm = TRUE) * 100,
+            C10_ACT1564_CS3 = sum(C10_ACT1564_CS3, na.rm = TRUE) / sum(C10_ACT1564, na.rm = TRUE) * 100,
+            C15_ACT1564_CS3 = sum(C15_ACT1564_CS3, na.rm = TRUE) / sum(C15_ACT1564, na.rm = TRUE) * 100,
+            C90_ACT1564_CS4 = sum(AT90TPI, na.rm = TRUE) / sum(AT90TA, na.rm = TRUE) * 100,
+            C99_ACT1564_CS4 = sum(AT99TPI, na.rm = TRUE) / sum(AT99TA, na.rm = TRUE) * 100,
+            C10_ACT1564_CS4 = sum(C10_ACT1564_CS4, na.rm = TRUE) / sum(C10_ACT1564, na.rm = TRUE) * 100,
+            C15_ACT1564_CS4 = sum(C15_ACT1564_CS4, na.rm = TRUE) / sum(C15_ACT1564, na.rm = TRUE) * 100,
+            C90_ACT1564_CS5 = sum(AT90TEM, na.rm = TRUE) / sum(AT90TA, na.rm = TRUE) * 100,
+            C99_ACT1564_CS5 = sum(AT99TEM, na.rm = TRUE) / sum(AT99TA, na.rm = TRUE) * 100,
+            C10_ACT1564_CS5 = sum(C10_ACT1564_CS5, na.rm = TRUE) / sum(C10_ACT1564, na.rm = TRUE) * 100,
+            C15_ACT1564_CS5 = sum(C15_ACT1564_CS5, na.rm = TRUE) / sum(C15_ACT1564, na.rm = TRUE) * 100,
+            C90_ACT1564_CS6 = sum(AT90TOU, na.rm = TRUE) / sum(AT90TA, na.rm = TRUE) * 100,
+            C99_ACT1564_CS6 = sum(AT99TOU, na.rm = TRUE) / sum(AT99TA, na.rm = TRUE) * 100,
+            C10_ACT1564_CS6 = sum(C10_ACT1564_CS6, na.rm = TRUE) / sum(C10_ACT1564, na.rm = TRUE) * 100,
+            C15_ACT1564_CS6 = sum(C15_ACT1564_CS6, na.rm = TRUE) / sum(C15_ACT1564, na.rm = TRUE) * 100,) %>%
   mutate(id = 75)
+
+# 5e arr
+empl75105 <- empl %>%
+  filter(COM == '75105') %>%
+  group_by(COM) %>%
+  summarise(C90_ACT1564_CS1 = sum(AT90TAG, na.rm = TRUE) / sum(AT90TA, na.rm = TRUE) * 100,
+            C99_ACT1564_CS1 = sum(AT99TAG, na.rm = TRUE) / sum(AT99TA, na.rm = TRUE) * 100,
+            C10_ACT1564_CS1 = sum(C10_ACT1564_CS1, na.rm = TRUE) / sum(C10_ACT1564, na.rm = TRUE) * 100,
+            C15_ACT1564_CS1 = sum(C15_ACT1564_CS1, na.rm = TRUE) / sum(C15_ACT1564, na.rm = TRUE) * 100,
+            C90_ACT1564_CS2 = sum(AT90TAR, na.rm = TRUE) / sum(AT90TA, na.rm = TRUE) * 100,
+            C99_ACT1564_CS2 = sum(AT99TAR, na.rm = TRUE) / sum(AT99TA, na.rm = TRUE) * 100,
+            C10_ACT1564_CS2 = sum(C10_ACT1564_CS2, na.rm = TRUE) / sum(C10_ACT1564, na.rm = TRUE) * 100,
+            C15_ACT1564_CS2 = sum(C15_ACT1564_CS2, na.rm = TRUE) / sum(C15_ACT1564, na.rm = TRUE) * 100,
+            C90_ACT1564_CS3 = sum(AT90TCA, na.rm = TRUE) / sum(AT90TA, na.rm = TRUE) * 100,
+            C99_ACT1564_CS3 = sum(AT99TCA, na.rm = TRUE) / sum(AT99TA, na.rm = TRUE) * 100,
+            C10_ACT1564_CS3 = sum(C10_ACT1564_CS3, na.rm = TRUE) / sum(C10_ACT1564, na.rm = TRUE) * 100,
+            C15_ACT1564_CS3 = sum(C15_ACT1564_CS3, na.rm = TRUE) / sum(C15_ACT1564, na.rm = TRUE) * 100,
+            C90_ACT1564_CS4 = sum(AT90TPI, na.rm = TRUE) / sum(AT90TA, na.rm = TRUE) * 100,
+            C99_ACT1564_CS4 = sum(AT99TPI, na.rm = TRUE) / sum(AT99TA, na.rm = TRUE) * 100,
+            C10_ACT1564_CS4 = sum(C10_ACT1564_CS4, na.rm = TRUE) / sum(C10_ACT1564, na.rm = TRUE) * 100,
+            C15_ACT1564_CS4 = sum(C15_ACT1564_CS4, na.rm = TRUE) / sum(C15_ACT1564, na.rm = TRUE) * 100,
+            C90_ACT1564_CS5 = sum(AT90TEM, na.rm = TRUE) / sum(AT90TA, na.rm = TRUE) * 100,
+            C99_ACT1564_CS5 = sum(AT99TEM, na.rm = TRUE) / sum(AT99TA, na.rm = TRUE) * 100,
+            C10_ACT1564_CS5 = sum(C10_ACT1564_CS5, na.rm = TRUE) / sum(C10_ACT1564, na.rm = TRUE) * 100,
+            C15_ACT1564_CS5 = sum(C15_ACT1564_CS5, na.rm = TRUE) / sum(C15_ACT1564, na.rm = TRUE) * 100,
+            C90_ACT1564_CS6 = sum(AT90TOU, na.rm = TRUE) / sum(AT90TA, na.rm = TRUE) * 100,
+            C99_ACT1564_CS6 = sum(AT99TOU, na.rm = TRUE) / sum(AT99TA, na.rm = TRUE) * 100,
+            C10_ACT1564_CS6 = sum(C10_ACT1564_CS6, na.rm = TRUE) / sum(C10_ACT1564, na.rm = TRUE) * 100,
+            C15_ACT1564_CS6 = sum(C15_ACT1564_CS6, na.rm = TRUE) / sum(C15_ACT1564, na.rm = TRUE) * 100,) %>%
+  rename(id = COM)
+
+# Our Neighborhood
+empl751050000 <- empl %>%
+  filter(IRIS %in% c('751052003', '751051905', '751051906')) %>%
+  summarise(C90_ACT1564_CS1 = sum(AT90TAG, na.rm = TRUE) / sum(AT90TA, na.rm = TRUE) * 100,
+            C99_ACT1564_CS1 = sum(AT99TAG, na.rm = TRUE) / sum(AT99TA, na.rm = TRUE) * 100,
+            C10_ACT1564_CS1 = sum(C10_ACT1564_CS1, na.rm = TRUE) / sum(C10_ACT1564, na.rm = TRUE) * 100,
+            C15_ACT1564_CS1 = sum(C15_ACT1564_CS1, na.rm = TRUE) / sum(C15_ACT1564, na.rm = TRUE) * 100,
+            C90_ACT1564_CS2 = sum(AT90TAR, na.rm = TRUE) / sum(AT90TA, na.rm = TRUE) * 100,
+            C99_ACT1564_CS2 = sum(AT99TAR, na.rm = TRUE) / sum(AT99TA, na.rm = TRUE) * 100,
+            C10_ACT1564_CS2 = sum(C10_ACT1564_CS2, na.rm = TRUE) / sum(C10_ACT1564, na.rm = TRUE) * 100,
+            C15_ACT1564_CS2 = sum(C15_ACT1564_CS2, na.rm = TRUE) / sum(C15_ACT1564, na.rm = TRUE) * 100,
+            C90_ACT1564_CS3 = sum(AT90TCA, na.rm = TRUE) / sum(AT90TA, na.rm = TRUE) * 100,
+            C99_ACT1564_CS3 = sum(AT99TCA, na.rm = TRUE) / sum(AT99TA, na.rm = TRUE) * 100,
+            C10_ACT1564_CS3 = sum(C10_ACT1564_CS3, na.rm = TRUE) / sum(C10_ACT1564, na.rm = TRUE) * 100,
+            C15_ACT1564_CS3 = sum(C15_ACT1564_CS3, na.rm = TRUE) / sum(C15_ACT1564, na.rm = TRUE) * 100,
+            C90_ACT1564_CS4 = sum(AT90TPI, na.rm = TRUE) / sum(AT90TA, na.rm = TRUE) * 100,
+            C99_ACT1564_CS4 = sum(AT99TPI, na.rm = TRUE) / sum(AT99TA, na.rm = TRUE) * 100,
+            C10_ACT1564_CS4 = sum(C10_ACT1564_CS4, na.rm = TRUE) / sum(C10_ACT1564, na.rm = TRUE) * 100,
+            C15_ACT1564_CS4 = sum(C15_ACT1564_CS4, na.rm = TRUE) / sum(C15_ACT1564, na.rm = TRUE) * 100,
+            C90_ACT1564_CS5 = sum(AT90TEM, na.rm = TRUE) / sum(AT90TA, na.rm = TRUE) * 100,
+            C99_ACT1564_CS5 = sum(AT99TEM, na.rm = TRUE) / sum(AT99TA, na.rm = TRUE) * 100,
+            C10_ACT1564_CS5 = sum(C10_ACT1564_CS5, na.rm = TRUE) / sum(C10_ACT1564, na.rm = TRUE) * 100,
+            C15_ACT1564_CS5 = sum(C15_ACT1564_CS5, na.rm = TRUE) / sum(C15_ACT1564, na.rm = TRUE) * 100,
+            C90_ACT1564_CS6 = sum(AT90TOU, na.rm = TRUE) / sum(AT90TA, na.rm = TRUE) * 100,
+            C99_ACT1564_CS6 = sum(AT99TOU, na.rm = TRUE) / sum(AT99TA, na.rm = TRUE) * 100,
+            C10_ACT1564_CS6 = sum(C10_ACT1564_CS6, na.rm = TRUE) / sum(C10_ACT1564, na.rm = TRUE) * 100,
+            C15_ACT1564_CS6 = sum(C15_ACT1564_CS6, na.rm = TRUE) / sum(C15_ACT1564, na.rm = TRUE) * 100,) %>%
+  mutate(id = 751050000)
+
+# Our IRIS
+emplIRIS <- empl %>%
+  filter(IRIS %in% c('751052003', '751051905', '751051906')) %>%
+  group_by(IRIS) %>%
+  summarise(C90_ACT1564_CS1 = sum(AT90TAG, na.rm = TRUE) / sum(AT90TA, na.rm = TRUE) * 100,
+            C99_ACT1564_CS1 = sum(AT99TAG, na.rm = TRUE) / sum(AT99TA, na.rm = TRUE) * 100,
+            C10_ACT1564_CS1 = sum(C10_ACT1564_CS1, na.rm = TRUE) / sum(C10_ACT1564, na.rm = TRUE) * 100,
+            C15_ACT1564_CS1 = sum(C15_ACT1564_CS1, na.rm = TRUE) / sum(C15_ACT1564, na.rm = TRUE) * 100,
+            C90_ACT1564_CS2 = sum(AT90TAR, na.rm = TRUE) / sum(AT90TA, na.rm = TRUE) * 100,
+            C99_ACT1564_CS2 = sum(AT99TAR, na.rm = TRUE) / sum(AT99TA, na.rm = TRUE) * 100,
+            C10_ACT1564_CS2 = sum(C10_ACT1564_CS2, na.rm = TRUE) / sum(C10_ACT1564, na.rm = TRUE) * 100,
+            C15_ACT1564_CS2 = sum(C15_ACT1564_CS2, na.rm = TRUE) / sum(C15_ACT1564, na.rm = TRUE) * 100,
+            C90_ACT1564_CS3 = sum(AT90TCA, na.rm = TRUE) / sum(AT90TA, na.rm = TRUE) * 100,
+            C99_ACT1564_CS3 = sum(AT99TCA, na.rm = TRUE) / sum(AT99TA, na.rm = TRUE) * 100,
+            C10_ACT1564_CS3 = sum(C10_ACT1564_CS3, na.rm = TRUE) / sum(C10_ACT1564, na.rm = TRUE) * 100,
+            C15_ACT1564_CS3 = sum(C15_ACT1564_CS3, na.rm = TRUE) / sum(C15_ACT1564, na.rm = TRUE) * 100,
+            C90_ACT1564_CS4 = sum(AT90TPI, na.rm = TRUE) / sum(AT90TA, na.rm = TRUE) * 100,
+            C99_ACT1564_CS4 = sum(AT99TPI, na.rm = TRUE) / sum(AT99TA, na.rm = TRUE) * 100,
+            C10_ACT1564_CS4 = sum(C10_ACT1564_CS4, na.rm = TRUE) / sum(C10_ACT1564, na.rm = TRUE) * 100,
+            C15_ACT1564_CS4 = sum(C15_ACT1564_CS4, na.rm = TRUE) / sum(C15_ACT1564, na.rm = TRUE) * 100,
+            C90_ACT1564_CS5 = sum(AT90TEM, na.rm = TRUE) / sum(AT90TA, na.rm = TRUE) * 100,
+            C99_ACT1564_CS5 = sum(AT99TEM, na.rm = TRUE) / sum(AT99TA, na.rm = TRUE) * 100,
+            C10_ACT1564_CS5 = sum(C10_ACT1564_CS5, na.rm = TRUE) / sum(C10_ACT1564, na.rm = TRUE) * 100,
+            C15_ACT1564_CS5 = sum(C15_ACT1564_CS5, na.rm = TRUE) / sum(C15_ACT1564, na.rm = TRUE) * 100,
+            C90_ACT1564_CS6 = sum(AT90TOU, na.rm = TRUE) / sum(AT90TA, na.rm = TRUE) * 100,
+            C99_ACT1564_CS6 = sum(AT99TOU, na.rm = TRUE) / sum(AT99TA, na.rm = TRUE) * 100,
+            C10_ACT1564_CS6 = sum(C10_ACT1564_CS6, na.rm = TRUE) / sum(C10_ACT1564, na.rm = TRUE) * 100,
+            C15_ACT1564_CS6 = sum(C15_ACT1564_CS6, na.rm = TRUE) / sum(C15_ACT1564, na.rm = TRUE) * 100,) %>%
+  rename(id = IRIS)
+
+empl <- empl75 %>%
+  full_join(empl75105) %>%
+  full_join(empl751050000) %>%
+  full_join(emplIRIS) %>%
+  select(id, everything())
